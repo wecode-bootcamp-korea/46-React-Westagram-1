830993535b0381937993fc7e87bb06c358d075c9
shfkd123 <shfkd123@gmail.com>
2023-05-17 15:35:07 +0900

    ADD : README.md 파일 추가

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sample from './pages/minji/Sample/Sample';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Sample />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/minji/Sample/Sample.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Sample() {
+  return <h1>hi</h1>;
+}
+
+export default Sample;
