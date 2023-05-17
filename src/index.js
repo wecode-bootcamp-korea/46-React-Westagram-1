6cb930003e5843ba12c3eecdf72db30d500f43f5
shfkd123 <shfkd123@gmail.com>
2023-05-17 15:41:09 +0900

    ADD : index.js에서 scss파일 import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Sample from './pages/minji/Sample/Sample';
+import './styles/reset.scss';
+import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Sample />);
