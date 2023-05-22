eae50b9bd0ab153fbfaa06a3f0eb7df2c04d891d
shfkd123 <shfkd123@gmail.com>
2023-05-22 12:27:08 +0900

    Assignment 1 | Aside에서 상수 데이터 활용

---
+++ src/pages/minji/uiData/tag.js
@@ -0,0 +1,50 @@
+export const TAG_LIST = [
+  {
+    id: 0,
+    tagName: 'Instagram',
+  },
+  {
+    id: 1,
+    tagName: '정보',
+  },
+  {
+    id: 2,
+    tagName: '지원',
+  },
+  {
+    id: 3,
+    tagName: '홍보센터',
+  },
+  {
+    id: 4,
+    tagName: 'API',
+  },
+  {
+    id: 5,
+    tagName: '채용정보',
+  },
+  {
+    id: 6,
+    tagName: '개인정보처리방침',
+  },
+  {
+    id: 7,
+    tagName: '약관',
+  },
+  {
+    id: 8,
+    tagName: '디렉토리',
+  },
+  {
+    id: 9,
+    tagName: '프로필',
+  },
+  {
+    id: 10,
+    tagName: '해시태그',
+  },
+  {
+    id: 11,
+    tagName: '언어',
+  },
+];
