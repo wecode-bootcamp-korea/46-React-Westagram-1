c10b801e0ab4a0ad4701ff29b62d548d1a32fae0
zosuzin <zosuzin@gmail.com>
2023-05-23 18:56:16 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/suzin/Login/LoginSuzin.js
+++ src/pages/suzin/Login/LoginSuzin.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import './LoginSuzin.scss';
 
 const LoginSuzin = () => {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
   const navigate = useNavigate();
+  const saveUserId = e => {
+    setId(e.target.value);
+    console.log(id);
+  };
+  const saveUserPw = e => {
+    setPw(e.target.value);
+    console.log(pw);
+  };
   return (
     <div id="loginking">
       <main id="loginBox">
@@ -13,15 +23,21 @@ const LoginSuzin = () => {
           type="text"
           className="id"
           placeholder="전화번호, 사용자 이름 또는 이메일 "
+          onChange={saveUserId}
         />
-        <input type="text" className="password" placeholder="비밀번호" />
+        <input
+          type="text"
+          className="password"
+          placeholder="비밀번호"
+          onChange={saveUserPw}
+        />
         {/* <Link to="/main">
           <button class="buttonBox">로그인</button>
         </Link> */}
         <button
           class="buttonBox"
           onClick={() => {
-            navigate('/main');
+            navigate('/mainsuzin');
           }}
         >
           로그인
